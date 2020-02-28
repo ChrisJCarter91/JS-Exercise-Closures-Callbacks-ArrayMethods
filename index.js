5f314cc04ee33be83dba7fadafd488564fedaeff
Chris Carter <chris.j.carter91@gmail.com>
2020-02-27 22:28:12 -0700

    Having issues with runners, moving on

--- index.js
+++ index.js
@@ -178,8 +178,13 @@ function processDuplicateFree(list, callback) {
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  let names = [];
+  console.log(runners);
+  runners.forEach(function(runner){
+    return names.push(`${runner.last_name} ${runner.first_name}`);
+  });
+  return names;
 }
 
 /**
@@ -194,8 +199,8 @@ function getFullNames(/* CODE HERE */) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  return runners.map(runner => runner.first_name.toUpperCase());
 }
 
 /**
@@ -211,8 +216,8 @@ function firstNamesAllCaps(/* CODE HERE */) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, tShirtSize) {
+  return runners.filter(runners => runners.shirt_size === tShirtSize);
 }
 
 /**
@@ -225,8 +230,10 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  return runners.reduce((total, array) => {
+    return total + array.donation;
+  }, 0);
 }
 
 /////////////// CLOSURES ///////////////
